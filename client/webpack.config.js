bf9f94751276996f11645f4ff10eb531702d2f9f
alam2tg <aaronlamfinance@gmail.com>
2023-08-29 08:32:52 -0700

    Add Plugins + Modules to webpack.config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,12 +17,55 @@ module.exports = () => {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    //add new InjectManifest + new WebpackPwaManifest. ref 19.
     plugins: [
-      
+      new HtmlWebpackPlugin({ 
+        template: './src/index.html',
+        }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'service-worker.js'
+      }),
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Text Editor Progressive Web App',
+        crossorigin: 'use-credentials', //can be null, use-credentials or anonymous
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512] // multiple sizes
+          },
+          {
+            src: path.resolve('src/assets/large-icon.png'),
+            size: '1024x1024' // you can also use the specifications pattern
+          },
+          {
+            src: path.resolve('src/assets/maskable-icon.png'),
+            size: '1024x1024',
+            purpose: 'maskable'
+          }
+        ]
+      })
     ],
-
     module: {
       rules: [
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        // bower_components? check if part of package, supposedly deprecated. 
+        // didn't find bower components folder in @babel
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        }
         
       ],
     },
